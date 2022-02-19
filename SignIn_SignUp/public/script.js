ceedb9233dd00f9c212ac3147e63a4db221977d8
Krishna Teja <78008005+Kcode17@users.noreply.github.com>
2022-02-19 11:01:19 -0500

    Added regex for password validation

--- SignIn_SignUp/public/script.js
+++ SignIn_SignUp/public/script.js
@@ -1,39 +1,32 @@
 (function () {
-    'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-          if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-          }
-  
-          form.classList.add('was-validated')
-        }, false)
-      })
-  })()
+  'use strict'
 
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
 
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
 
-  function validateForm() {
-    let pw = document.forms["signIn"]["Password"].value;
-    console.log(pw.length);
-    if (pw == "") {
-        let textElement = document.getElementById('pwMessage');
-        textElement.innerHTML += 'Please enter a valid password.!';
-    }
-    
-    else if ( pw.length <= 8 ){
-        
-        let textElement = document.getElementById('pwMessage');
-        textElement.innerHTML += 'Password lenght should be more than 8 characters.!';
-        
-    }
-    return false;
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
 
+function passwdCheck(text){
+  var regexcheck = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*?&])([a-zA-Z0-9@$!%*?&]{8,})$/;
+  if(text.value.match(regexcheck))
+  {
+    let textElement = document.getElementById('pwMessage');
+    textElement.innerHTML += 'Password accepted';
+  }
+  else{
+    let textElement = document.getElementById('pwMessage');
+    textElement.innerHTML += 'Password does not meet the minimum criteria.!';
+  }
 }
