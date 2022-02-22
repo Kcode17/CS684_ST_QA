a7fd7c2ddedfea21a8d7bd7712104280fabd3d65
zrangwala1 <94558256+zrangwala1@users.noreply.github.com>
2022-02-22 09:35:07 -0500

    Minor changes in validation message and user field properties

--- models/User.js
+++ models/User.js
@@ -4,18 +4,24 @@ const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
+    trim:true,
+    min: 8,
+    max: 20,
   },
   email: {
     type: String,
     required: true,
+    trim:true,
   },
   password: {
     type: String,
     required: true,
+    trim:true,
   },
   confirmpassword: {
     type: String,
     required: true,
+    trim:true,
   },
 });
 
